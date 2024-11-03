4e121b8e288b080e6a317bbc6705528331d5c933
Martin Gomez <martin_gomez_1996@hotmail.com>
2024-11-03 10:01:50 -0300

    Hago cambios esteticos en el navbar y añado un logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom"
 import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <Navbar/>
+    <BrowserRouter>
+      <Navbar/>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,47 @@
-import React from "react";
+import {Link} from "react-router-dom";
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Offcanvas navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr className="dropdown-divider"/>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="#">
+          <img src="../../public/logo/Logo.png" alt="StudyMarket" width="40" height="42"/>
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-start flex-grow-1 pe-3 mt-2">
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Productos
+                </Link>
+                <ul className="dropdown-menu">
+                  <li><Link className="dropdown-item" to="/">Apuntes</Link></li>
+                  <li><Link className="dropdown-item" to="/">Libros</Link></li>           
+                </ul>
               </li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Contaco</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Ayuda</Link>
+              </li>
             </ul>
-          </li>
-        </ul>
-        <form className="d-flex mt-3" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+            <form className="d-flex mt-2" role="search">
+              <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
+              <button className="btn btn-outline-success" type="submit">Buscar</button>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
+    </nav>
   );
 };
 
