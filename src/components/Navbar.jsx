ba7e9b53ae755d07ad74752fd5aaf9cf594ec315
SamyazaCode <lautaropaz__@hotmail.com>
2024-11-05 01:29:21 -0300

    Agrego detalles en Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary absolute-top">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="#">
+        <Link className="navbar-brand navbar-brand-spacing" to="/">
           <img src="../../public/logo/Logo.png" alt="StudyMarket" width="40" height="42"/>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
@@ -16,27 +16,27 @@ const Navbar = () => {
             <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-start flex-grow-1 pe-3 mt-2">
+            <ul className="navbar-nav navbar-spacing justify-content-start flex-grow-1 pe-3 mt-2">
               <li className="nav-item">
-                <Link className="nav-link" to="/">Inicio</Link>
+                <Link className="nav-link nav-link-custom" to="/">Inicio</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/tienda">Tienda</Link>
+                <Link className="nav-link nav-link-custom" to="/tienda">Tienda</Link>
               </li>
               <li className="nav-item dropdown">
-                <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link className="nav-link nav-link-custom dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Productos
                 </Link>
                 <ul className="dropdown-menu">
-                  <li><Link className="dropdown-item" to="/">Apuntes</Link></li>
-                  <li><Link className="dropdown-item" to="/">Libros</Link></li>           
+                  <li><Link className="dropdown-item nav-link-custom" to="/">Apuntes</Link></li>
+                  <li><Link className="dropdown-item nav-link-custom" to="/">Libros</Link></li>           
                 </ul>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/conocenos">Conocenos</Link>
+                <Link className="nav-link nav-link-custom" to="/conocenos">Conócenos</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/ayuda">Ayuda</Link>
+                <Link className="nav-link nav-link-custom" to="/ayuda">Ayuda</Link>
               </li>
             </ul>
             <form className="d-flex mt-2" role="search">
