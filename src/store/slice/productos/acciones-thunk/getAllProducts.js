8cc5884119358d0d0a275ca6eaf50f00c44e1a53
Martin <martin_gomez_1996@hotmail.com>
2024-11-20 19:37:18 -0300

    Hago funcionar el json server

--- src/store/slice/productos/acciones-thunk/getAllProducts.js
+++ src/store/slice/productos/acciones-thunk/getAllProducts.js
@@ -1,17 +1,17 @@
 import { setProductos } from ".."
-// import handleHttp from "../../../../utils/handle-peticionhttp"
+import handleHttp from "../../../../utils/handle-peticionhttp"
 import data from "../../../../data/data.json"
 import { pedirDatos } from "../../../../helpers/pedirDatos"
 
 export const getAllProducts = () => async ( dispatch ) => {
-    const url = data
+    const url = import.meta.env.VITE_ENDPOINT_BACKEND 
 
-    console.log(data)
-
+    
     try {
+        console.log(url)
         // Arreglo de bug productos (elegir)
-        // const data = await handleHttp(url)
-        const data = await pedirDatos(url)
+         const data = await handleHttp(url + "productos")
+        /* const data = await pedirDatos(url) */
         dispatch(setProductos(data))
         
     } catch (error) {
